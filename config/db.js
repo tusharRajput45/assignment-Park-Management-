7e38cac579094fad6a1d70723c63fef64e872027
tusharRajput45 <111507866+tusharRajput45@users.noreply.github.com>
2024-06-30 09:23:21 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://127.0.0.1:27017/parkDB")
+  .connect("mongodb+srv://tusharrajput919:window@test-usm.p4ufnvq.mongodb.net/ParkingDB?retryWrites=true&w=majority")
   .then(() => {
     console.log("Database succesfully connect");
   })
